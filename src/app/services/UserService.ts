786ff370e245b4d6645ef607af055eda2164fe9c
matheuspeluca <matheuspeluca2@gmail.com>
2023-09-15 22:07:53 -0300

    Adding POST for user creation

--- src/app/services/UserService.ts
+++ src/app/services/UserService.ts
@@ -1,22 +1,42 @@
 import * as Yup from 'yup'
 import {User} from '../models/User.model'
 import knex from '../../config/knex';
+import bcrypt from 'bcrypt'
+import AppError from '../../errors/AppError'
 
 interface Request {
+    login: string
     email: string
     password: string
-    //add demais campos
+    name: string
 }
 
 const schema = Yup.object().shape({
     email: Yup.string().email().required(),
+    login: Yup.string().required(),
+    name: Yup.string().required(),
     password: Yup.string().required().min(6),
 })
 
 class UserService {
 
     public async create(req: Request) {
+        if (!(await schema.isValid(req))) {
+            throw new AppError('Validation fails')
+        }
   
+        try {
+            const user = await knex<User>('users').insert({
+                name: req.name,
+                login: req.login,
+                email: req.email,
+                password: bcrypt.hashSync(req.password, 8)
+            }, 'id');
+            return user;
+            
+        } catch (error) {
+            throw new AppError(error)
+        }
     }
   }
   

79e1beb06387ffb12fec6613b31e369235f0d32d
Reinaldo Oliveira Machado Jr <reinaldo.m.jr@gmail.com>
2023-09-25 19:45:21 -0300

    Finished the API, and included the POSTMAN file

--- knexfile.ts
+++ knexfile.ts
@@ -20,6 +20,23 @@ const config: {[key: string]: Knex.Config} = {
       directory: './database/migrations',
     }
   },
+  production: {
+    client: 'postgres',
+    connection: {
+      host: process.env.DB_HOST,
+      port: 5432,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      ssl: { rejectUnauthorized: false }
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+    migrations: {
+      directory: './database/migrations',
+    }
+  }
 };
 
 export default config;
